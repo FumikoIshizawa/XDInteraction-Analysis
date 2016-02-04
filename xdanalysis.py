d1873fe9ab0041f5020104019602a41b954239fa
FumikoIshizawa <p.krrr.731@gmail.com>
2016-02-04 17:06:18 +0900

    Distinguishment users

--- xdanalysis.py
+++ xdanalysis.py
@@ -6,7 +6,6 @@ import os
 import xlsxwriter
 
 workbook = xlsxwriter.Workbook('data/log.xlsx')
-worksheet = workbook.add_worksheet()
 
 gestures = [
 	"GyroUp", 
@@ -26,38 +25,43 @@ gestures = [
 def load_file(file_name):
 	f = open('logs/' + file_name, 'r')
 	line = f.readline()
-	colum_num = 0
-	pre_data = {
-		"SwipeLeft":{"action":"Next Page","window":0},
-		"GyroUp":{"action":"Scroll Up","window":0},
-		"ButtonLeft":{"action":"No Gesture","window":0},
-		"SwipeDown":{"action":"Text Small","window":0},
-		"SingleTap":{"action":"No Gesture","window":0},
-		"SwipeUp":{"action":"Text Big","window":0},
-		"PinchIn":{"action":"Zoom In","window":0},
-		"SwipeRight":{"action":"Back Page","window":0},
-		"PinchOut":{"action":"Zoom Out","window":0},
-		"DoubleTap":{"action":"No Gesture","window":0},
-		"GyroDown":{"action":"Scroll Down","window":0},
-		"ButtonRight":{"action":"No Gesture","window":0}
-	}
 	name = "NoName"
+	worksheets = {}
+	worksheets_num = {}
+	pre_data = {}
 
 	while line:
 		if line[0] == "[":
 			name = load_name(line)
+			if not worksheets.has_key(name):
+				worksheets[name] = workbook.add_worksheet()
+				worksheets_num[name] = 0
+				pre_data[name] = {
+					"SwipeLeft":{"action":"Next Page","window":0},
+					"GyroUp":{"action":"Scroll Up","window":0},
+					"ButtonLeft":{"action":"No Gesture","window":0},
+					"SwipeDown":{"action":"Text Small","window":0},
+					"SingleTap":{"action":"No Gesture","window":0},
+					"SwipeUp":{"action":"Text Big","window":0},
+					"PinchIn":{"action":"Zoom In","window":0},
+					"SwipeRight":{"action":"Back Page","window":0},
+					"PinchOut":{"action":"Zoom Out","window":0},
+					"DoubleTap":{"action":"No Gesture","window":0},
+					"GyroDown":{"action":"Scroll Down","window":0},
+					"ButtonRight":{"action":"No Gesture","window":0}
+				}
 		elif line[0] == "{":
 			data = json.loads(line)
-			write_data(data, name, colum_num, pre_data)
-			colum_num += 4
-			pre_data = data
+			write_data(worksheets[name], data, name, worksheets_num[name], pre_data[name])
+			worksheets_num[name] += 4
+			pre_data[name] = data
 		line = f.readline()
 	f.close()
 
 def load_name(line):
 	return line[39:]
 
-def write_data(data, name, colum_num, pre_data):
+def write_data(worksheet, data, name, colum_num, pre_data):
 	diff = workbook.add_format()
 	diff.set_bg_color('yellow')
 
